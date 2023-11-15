b19511253c1ac344943db2c8640e2a936a5c5af2
Kim Lasse Massesson <kima4@elev.tec.dk>
2023-11-15 12:49:49 +0100

    Add project files.

---
+++ Loops.cs
@@ -0,0 +1,14 @@
+﻿namespace ToolboxH1
+{
+    internal class Loops
+    {
+        public static void ForMethodWithModulus()
+        {
+            for (int i = 0; i < 1000; i+=10)
+            {
+                Console.WriteLine(i % 360);
+            }
+        }
+
+    }
+}
